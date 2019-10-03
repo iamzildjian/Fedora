2b8e19739cb6b1007318cc0f2e9f2e7aaa1074e6
Zildjian <jepfarragona@gmail.com>
2019-10-03 20:19:42 +0800

    Update and rename bash.sh to obfs.sh

--- obfs.sh
+++ obfs.sh
@@ -9,7 +9,7 @@ apt-get update > /dev/null 2>&1
 apt-get upgrade > /dev/null 2>&1
 apt-get install unzip > /dev/null 2>&1
 apt-get install build-essential > /dev/null 2>&1
-wget  http://phcyber.com/scripts/shc-release.zip > /dev/null 2>&1
+wget  https://github.com/iamzildjian/Fedora/raw/master/shc-release.zip > /dev/null 2>&1
 unzip shc-release.zip  > /dev/null 2>&1
 cd shc-release
 ./configure  > /dev/null 2>&1
